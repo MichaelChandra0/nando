73b778f177fe16a24e1be2e276de23c7f53d1309
MichaelChandra0 <michaelchandra700@gmail.com>
2025-02-18 23:33:07 +0700

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const submitButton = document.getElementById("submit-button");
+
+  submitButton.addEventListener("click", function () {
+    document.body.style.backgroundColor = "lightblue";
+  });
+});
